020ba62810203565c3bcbda7e33810b5cd2302d4
Mokshith S <102305359+moki-s@users.noreply.github.com>
2024-04-02 01:13:33 +0530

    caesar.py

---
+++ caesar.py
@@ -0,0 +1,51 @@
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+
+print(logo)
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+
+def caesar(start_text, shift_amt, cipher_direction):
+  end_text = ""
+  if cipher_direction ==  "decode":
+    shift_amt *= -1
+  for char in start_text:
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_pos = (position+shift_amt)%26
+      end_text += alphabet[new_pos]
+    else:
+      end_text += char  
+  print(f"Here's the {direction}d result: {end_text}")
+
+should_cont = True
+while should_cont:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+
+
+  caesar(start_text= text, shift_amt= shift, cipher_direction= direction)    
+  result = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+  if result == "no":
+    should_cont = False
+    print("Goodbye")
+
+
